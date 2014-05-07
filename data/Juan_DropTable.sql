7f6cc38e7479876890c2dd2769cf5ba4ed2800fe
Juan Tonelli <j.tonelli90@gmail.com>
2014-05-07 17:26:10 -0300

    Juan - Migración SQL - Prueba
    Subo los scripts de la migración pero no son los definitivos.

---
+++ data/Juan_DropTable.sql
@@ -0,0 +1,46 @@
+USE GD1C2014
+
+GO
+
+Drop Table J2LA.Clientes
+GO
+Drop Table J2LA.Empresas
+GO
+Drop Table J2LA.TiposDoc
+GO
+Drop Table J2LA.Facturas_Det
+GO
+Drop Table J2LA.Facturas
+GO
+Drop Table J2LA.Preguntas
+GO
+Drop Table J2LA.Compras
+GO
+Drop Table J2LA.Calificaciones
+GO
+Drop Table J2LA.Ofertas
+GO
+Drop Table J2LA.Publicaciones_Rubros
+GO
+Drop Table J2LA.Rubros
+GO
+Drop Table J2LA.Publicaciones
+GO
+Drop Table J2LA.Publicaciones_Visibilidades
+GO
+Drop Table J2LA.Publicaciones_Tipos
+GO
+Drop Table J2LA.Publicaciones_Estados
+GO
+Drop Table J2LA.Usuarios_Roles
+GO
+Drop Table J2LA.Usuarios
+GO
+Drop Table J2LA.Roles_Funcionalidades
+GO
+Drop Table J2LA.Funcionalidades
+GO
+Drop Table J2LA.Roles
+GO
+Drop Schema J2LA
+GO
