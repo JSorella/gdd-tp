291a84ef0c5179e59dd8f5db9ae5bb8a2174e237
luquitas120 <lukas.120@hotmail.com>
2014-05-26 22:04:02 -0300

    Forms de Listados Estadisticos

--- src/FrbaCommerce/Vistas/Listado_Estadístico/ListadoEstadistico.Designer.cs
+++ src/FrbaCommerce/Vistas/Listado_Estadístico/ListadoEstadistico.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace FrbaCommerce.Listado_Estadistico
+﻿namespace FrbaCommerce.Vistas.Listado_Estadistico
 {
     partial class ListadoEstadistico
     {
@@ -28,20 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gridListado = new System.Windows.Forms.DataGridView();
+            this.cboxListado = new System.Windows.Forms.ComboBox();
+            this.cboxAnio = new System.Windows.Forms.ComboBox();
+            this.cboxTrimestre = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cboxVisibilidad = new System.Windows.Forms.ComboBox();
+            this.cboxMes = new System.Windows.Forms.ComboBox();
+            this.labelVisibilidad = new System.Windows.Forms.Label();
+            this.labelMes = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridListado)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Año";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Trimestre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Seleccione listado";
+            // 
+            // gridListado
+            // 
+            this.gridListado.AllowUserToAddRows = false;
+            this.gridListado.AllowUserToDeleteRows = false;
+            this.gridListado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridListado.Location = new System.Drawing.Point(16, 319);
+            this.gridListado.Name = "gridListado";
+            this.gridListado.ReadOnly = true;
+            this.gridListado.Size = new System.Drawing.Size(626, 232);
+            this.gridListado.TabIndex = 6;
+            // 
+            // cboxListado
+            // 
+            this.cboxListado.FormattingEnabled = true;
+            this.cboxListado.Location = new System.Drawing.Point(114, 137);
+            this.cboxListado.Name = "cboxListado";
+            this.cboxListado.Size = new System.Drawing.Size(432, 21);
+            this.cboxListado.TabIndex = 7;
+            this.cboxListado.SelectedIndexChanged += new System.EventHandler(this.cboxListado_SelectedIndexChanged);
+            // 
+            // cboxAnio
+            // 
+            this.cboxAnio.FormattingEnabled = true;
+            this.cboxAnio.Location = new System.Drawing.Point(114, 39);
+            this.cboxAnio.Name = "cboxAnio";
+            this.cboxAnio.Size = new System.Drawing.Size(81, 21);
+            this.cboxAnio.TabIndex = 9;
+            // 
+            // cboxTrimestre
+            // 
+            this.cboxTrimestre.FormattingEnabled = true;
+            this.cboxTrimestre.Location = new System.Drawing.Point(114, 86);
+            this.cboxTrimestre.Name = "cboxTrimestre";
+            this.cboxTrimestre.Size = new System.Drawing.Size(81, 21);
+            this.cboxTrimestre.TabIndex = 10;
+            this.cboxTrimestre.SelectedIndexChanged += new System.EventHandler(this.cboxTrimestre_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(564, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 26);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Consultar!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cboxVisibilidad
+            // 
+            this.cboxVisibilidad.FormattingEnabled = true;
+            this.cboxVisibilidad.Location = new System.Drawing.Point(114, 184);
+            this.cboxVisibilidad.Name = "cboxVisibilidad";
+            this.cboxVisibilidad.Size = new System.Drawing.Size(207, 21);
+            this.cboxVisibilidad.TabIndex = 12;
+            this.cboxVisibilidad.SelectedIndexChanged += new System.EventHandler(this.cBoxVisibilidad_SelectedIndexChanged);
+            // 
+            // cboxMes
+            // 
+            this.cboxMes.FormattingEnabled = true;
+            this.cboxMes.Location = new System.Drawing.Point(114, 236);
+            this.cboxMes.Name = "cboxMes";
+            this.cboxMes.Size = new System.Drawing.Size(81, 21);
+            this.cboxMes.TabIndex = 13;
+            // 
+            // labelVisibilidad
+            // 
+            this.labelVisibilidad.AutoSize = true;
+            this.labelVisibilidad.Location = new System.Drawing.Point(44, 187);
+            this.labelVisibilidad.Name = "labelVisibilidad";
+            this.labelVisibilidad.Size = new System.Drawing.Size(53, 13);
+            this.labelVisibilidad.TabIndex = 14;
+            this.labelVisibilidad.Text = "Visibilidad";
+            // 
+            // labelMes
+            // 
+            this.labelMes.AutoSize = true;
+            this.labelMes.Location = new System.Drawing.Point(70, 239);
+            this.labelMes.Name = "labelMes";
+            this.labelMes.Size = new System.Drawing.Size(27, 13);
+            this.labelMes.TabIndex = 15;
+            this.labelMes.Text = "Mes";
             // 
             // ListadoEstadistico
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(653, 566);
+            this.Controls.Add(this.labelMes);
+            this.Controls.Add(this.labelVisibilidad);
+            this.Controls.Add(this.cboxMes);
+            this.Controls.Add(this.cboxVisibilidad);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cboxTrimestre);
+            this.Controls.Add(this.cboxAnio);
+            this.Controls.Add(this.cboxListado);
+            this.Controls.Add(this.gridListado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "ListadoEstadistico";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.ListadoEstadistico_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridListado)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView gridListado;
+        private System.Windows.Forms.ComboBox cboxListado;
+        private System.Windows.Forms.ComboBox cboxAnio;
+        private System.Windows.Forms.ComboBox cboxTrimestre;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cboxVisibilidad;
+        private System.Windows.Forms.ComboBox cboxMes;
+        private System.Windows.Forms.Label labelVisibilidad;
+        private System.Windows.Forms.Label labelMes;
     }
 }
--- src/FrbaCommerce/Vistas/Listado_Estadístico/ListadoEstadistico.cs
+++ src/FrbaCommerce/Vistas/Listado_Estadístico/ListadoEstadistico.cs
@@ -6,8 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using FrbaCommerce;
 
-namespace FrbaCommerce.Listado_Estadistico
+namespace FrbaCommerce.Vistas.Listado_Estadistico
 {
     public partial class ListadoEstadistico : Form
     {
@@ -18,7 +19,211 @@ namespace FrbaCommerce.Listado_Estadistico
 
         private void ListadoEstadistico_Load(object sender, EventArgs e)
         {
+            gridListado.Visible = false;
+            cargarComboBoxAños();
+            cargarComboBoxTrimestres();
+            cargarComboBoxListados();
+            cargarComboBoxVisibilidades();
+        }
+
+
+        public void cargarComboBoxAños()
+        {
+            List<KeyValuePair<string, int>> anio = new List<KeyValuePair<string, int>>();
+            //esto se podria flexibilizar trayendo los anios de las fechas de la base de datos, pero como la bdd no crece y estamos en el 2014.
+            anio.Add(generarKeyValueInt("2013", 2013));
+            anio.Add(generarKeyValueInt("2014", 2014));
+            cboxAnio.DisplayMember = "Key";
+            cboxAnio.ValueMember = "Value";
+            cboxAnio.DataSource = anio;
+        }
+
+        public void cargarComboBoxTrimestres()
+        {
+            List<KeyValuePair<string, int>> trimestre = new List<KeyValuePair<string, int>>();
+            trimestre.Add(generarKeyValueInt("Primero", 1));
+            trimestre.Add(generarKeyValueInt("Segundo", 2));
+            trimestre.Add(generarKeyValueInt("Tercero", 3));
+            trimestre.Add(generarKeyValueInt("Cuarto", 4));
+            cboxTrimestre.DisplayMember = "Key";
+            cboxTrimestre.ValueMember = "Value";
+            cboxTrimestre.DataSource = trimestre;
+        }
+
+        public void cargarComboBoxListados()
+        {
+            List<KeyValuePair<string, int>> listado = new List<KeyValuePair<string, int>>();
+            listado.Add(generarKeyValueInt("getTop5VendedoresConMayorCantDeProdNoVendidos", 1));
+            listado.Add(generarKeyValueInt("getTop5VendedoresConMayorFacturacion", 2));
+            listado.Add(generarKeyValueInt("getTop5VendedoresConMayoresCalificaciones", 3));
+            listado.Add(generarKeyValueInt("getTop5ClientesConMayorCantDePublicacionesSinCalificar", 4));
+            cboxListado.DisplayMember = "Key";
+            cboxListado.ValueMember = "Value";
+            cboxListado.DataSource = listado;
+        }
+
+        public void cargarComboBoxVisibilidades()
+        {
+            string query = "select * from J2LA.Publicaciones_Visibilidades";
+            Connection connect = new Connection();
+            DataTable tablaVisibilidades = connect.execute_query(query);
+            cboxVisibilidad.DataSource = tablaVisibilidades;
+            cboxVisibilidad.DisplayMember = "pubvis_Descripcion";
+            cboxVisibilidad.ValueMember = "pubvis_Codigo";
+        }
+
+        public void cargarComboBoxMeses()
+        {
+            List<KeyValuePair<string, int>> mes = new List<KeyValuePair<string, int>>();
+            if (cboxTrimestre.SelectedIndex == 0)
+            {
+                mes.Add(generarKeyValueInt("Enero", 1));
+                mes.Add(generarKeyValueInt("Febrero", 2));
+                mes.Add(generarKeyValueInt("Marzo", 3));
+            }
+            else
+            {
+                if (cboxTrimestre.SelectedIndex == 1)
+                {
+                    mes.Add(generarKeyValueInt("Abril", 4));
+                    mes.Add(generarKeyValueInt("Mayo", 5));
+                    mes.Add(generarKeyValueInt("Junio", 6));
+                }
+                else
+                {
+                    if (cboxTrimestre.SelectedIndex == 2)
+                    {
+                        mes.Add(generarKeyValueInt("Julio", 7));
+                        mes.Add(generarKeyValueInt("Agosto", 8));
+                        mes.Add(generarKeyValueInt("Septiembre", 9));
+                    }
+                    else
+                    {
+                        mes.Add(generarKeyValueInt("Octubre", 10));
+                        mes.Add(generarKeyValueInt("Noviembre", 11));
+                        mes.Add(generarKeyValueInt("Diciembre", 12));
+                    }
+                }
+            }
+            cboxMes.DisplayMember = "Key";
+            cboxMes.ValueMember = "Value";
+            cboxMes.DataSource = mes;
+        }
+
+        private KeyValuePair<string, int> generarKeyValueInt(string descripcion, int numero)
+        {
+            return new KeyValuePair<string, int>(descripcion, numero);
+        }
+
+        private int getAnio()
+        {
+            return ((KeyValuePair<string, int>)cboxAnio.SelectedItem).Value;
+        }
+
+        private int getTrimestre()
+        {
+            return ((KeyValuePair<string, int>)cboxTrimestre.SelectedItem).Value;
+        }
+
+        private int getVisibilidad()
+        {
+            return ((KeyValuePair<string, int>)cboxVisibilidad.SelectedItem).Value;
+        }
+
+        private int getMes()
+        {
+            return ((KeyValuePair<string, int>)cboxMes.SelectedItem).Value;
+        }
+
+        public DataTable getTop5VendedoresConMayorCantDeProdNoVendidos(int anio, int trimestre)
+        {
+            string query = "exec J2LA.getTop5VendedoresConMayorCantDeProdNoVendidos "+anio+", "+trimestre;
+            Connection connect = new Connection();
+            DataTable tablaListado = connect.execute_query(query);
+            return tablaListado;
+        }
+
+        public DataTable getTop5VendedoresConMayorFacturacion(int anio, int trimestre)
+        {
+            string query = "exec J2LA.getTop5VendedoresConMayorFacturacion " + anio + ", " + trimestre;
+            Connection connect = new Connection();
+            DataTable tablaListado = connect.execute_query(query);
+            return tablaListado;
+        }
+
+        public DataTable getTop5VendedoresConMayoresCalificaciones(int anio, int trimestre)
+        {
+            string query = "exec J2LA.getTop5VendedoresConMayoresCalificaciones " + anio + ", " + trimestre;
+            Connection connect = new Connection();
+            DataTable tablaListado = connect.execute_query(query);
+            return tablaListado;
+        }
+
+        public DataTable getTop5ClientesConMayorCantDePublicacionesSinCalificar(int anio, int trimestre)
+        {
+            string query = "exec J2LA.getTop5ClientesConMayorCantDePublicacionesSinCalificar " + anio + ", " + trimestre;
+            Connection connect = new Connection();
+            DataTable tablaListado = connect.execute_query(query);
+            return tablaListado;
+        }
+
+
+        private void cargarGrid(DataTable dt)
+        {
+            gridListado.Visible = true;
+            gridListado.DataSource = dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int anio = this.getAnio();
+            int trimestre = this.getTrimestre();
+            switch (cboxListado.SelectedIndex)
+            {
+                case 0:
+                    this.cargarGrid(getTop5VendedoresConMayorCantDeProdNoVendidos(anio, trimestre));
+                    break;
+                case 1:
+                    this.cargarGrid(getTop5VendedoresConMayorFacturacion(anio, trimestre));
+                    break;
+                case 2:
+                    this.cargarGrid(getTop5VendedoresConMayoresCalificaciones(anio, trimestre));
+                    break;
+                case 3:
+                    this.cargarGrid(getTop5ClientesConMayorCantDePublicacionesSinCalificar(anio, trimestre));
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void cBoxVisibilidad_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
         }
+
+        private void cboxListado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboxListado.SelectedIndex == 0)
+            {
+                cboxVisibilidad.Visible = true;
+                cboxMes.Visible = true;
+                labelVisibilidad.Visible = true;
+                labelMes.Visible = true;
+            }
+            else
+            {
+                cboxVisibilidad.Visible = false;
+                cboxMes.Visible = false;
+                labelVisibilidad.Visible = false;
+                labelMes.Visible = false;
+            }
+        }
+
+        private void cboxTrimestre_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cargarComboBoxMeses();
+        }
+
     }
 }
