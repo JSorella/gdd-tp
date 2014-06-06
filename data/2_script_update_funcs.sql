f31d7af4ccd1e48aa77fa88095e71905678b902b
luquitas120 <lukas.120@hotmail.com>
2014-06-06 01:28:25 -0300

    Fix 2_script_update_funcs.sql

--- data/2_script_update_funcs.sql
+++ data/2_script_update_funcs.sql
@@ -2,5 +2,5 @@ Insert Into J2LA.Funcionalidades(fun_nombre)
 	Values ('Cambiar Contraseña'), ('Baja Usuario'), ('Calificar Vendedor')
 Go
 Insert Into J2LA.Roles_Funcionalidades 
-	Values (1,13), (1,14), (1,15), (2, 13), (2, 15), (3, 15), (3, 15)
+	Values (1,13), (1,14), (1,15), (2, 13), (2, 15), (3, 15)
 Go
