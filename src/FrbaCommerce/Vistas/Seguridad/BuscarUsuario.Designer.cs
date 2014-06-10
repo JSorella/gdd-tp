8185e57b2c6d7f3fde6c41c133751a998337a1a0
Juan Tonelli <j.tonelli90@gmail.com>
2014-06-10 18:22:47 -0300

    Desarrollo
    Desarrollo

--- src/FrbaCommerce/Vistas/Seguridad/BuscarUsuario.Designer.cs
+++ src/FrbaCommerce/Vistas/Seguridad/BuscarUsuario.Designer.cs
@@ -28,20 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.grpParam = new System.Windows.Forms.GroupBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSeleccionar = new System.Windows.Forms.Button();
+            this.dgvUsu = new System.Windows.Forms.DataGridView();
+            this.grpParam.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsu)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSalir.Location = new System.Drawing.Point(290, 401);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // grpParam
+            // 
+            this.grpParam.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpParam.Controls.Add(this.txtNombre);
+            this.grpParam.Controls.Add(this.label1);
+            this.grpParam.Controls.Add(this.btnBuscar);
+            this.grpParam.Controls.Add(this.btnLimpiar);
+            this.grpParam.Location = new System.Drawing.Point(12, 12);
+            this.grpParam.Name = "grpParam";
+            this.grpParam.Size = new System.Drawing.Size(353, 100);
+            this.grpParam.TabIndex = 12;
+            this.grpParam.TabStop = false;
+            this.grpParam.Text = "Filtros de Busqueda";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNombre.Location = new System.Drawing.Point(116, 29);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(231, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre de Usuario :";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBuscar.Location = new System.Drawing.Point(272, 71);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(9, 71);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 3;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSeleccionar
+            // 
+            this.btnSeleccionar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSeleccionar.Location = new System.Drawing.Point(209, 401);
+            this.btnSeleccionar.Name = "btnSeleccionar";
+            this.btnSeleccionar.Size = new System.Drawing.Size(75, 23);
+            this.btnSeleccionar.TabIndex = 5;
+            this.btnSeleccionar.Text = "Seleccionar Rol";
+            this.btnSeleccionar.UseVisualStyleBackColor = true;
+            this.btnSeleccionar.Click += new System.EventHandler(this.btnSeleccionar_Click);
+            // 
+            // dgvUsu
+            // 
+            this.dgvUsu.AllowUserToAddRows = false;
+            this.dgvUsu.AllowUserToDeleteRows = false;
+            this.dgvUsu.AllowUserToOrderColumns = true;
+            this.dgvUsu.AllowUserToResizeRows = false;
+            this.dgvUsu.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvUsu.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvUsu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsu.Location = new System.Drawing.Point(12, 118);
+            this.dgvUsu.MultiSelect = false;
+            this.dgvUsu.Name = "dgvUsu";
+            this.dgvUsu.ReadOnly = true;
+            this.dgvUsu.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvUsu.Size = new System.Drawing.Size(353, 277);
+            this.dgvUsu.TabIndex = 4;
+            this.dgvUsu.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUsu_CellDoubleClick);
             // 
             // BuscarUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(377, 436);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.grpParam);
+            this.Controls.Add(this.btnSeleccionar);
+            this.Controls.Add(this.dgvUsu);
             this.Name = "BuscarUsuario";
             this.Text = "BuscarUsuario";
             this.Load += new System.EventHandler(this.BuscarUsuario_Load);
+            this.grpParam.ResumeLayout(false);
+            this.grpParam.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsu)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.GroupBox grpParam;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSeleccionar;
+        private System.Windows.Forms.DataGridView dgvUsu;
     }
 }
--- src/FrbaCommerce/Vistas/Seguridad/BuscarUsuario.cs
+++ src/FrbaCommerce/Vistas/Seguridad/BuscarUsuario.cs
@@ -11,14 +11,77 @@ namespace FrbaCommerce
 {
     public partial class BuscarUsuario : Form
     {
+        private DataRow mobjDrResultado; // Variable para pasar a los formularios que llamen a la busqueda.
+
         public BuscarUsuario()
         {
             InitializeComponent();
         }
 
+        public System.Data.DataRow Resultado
+        {
+            get { return mobjDrResultado; }
+        }
+
         private void BuscarUsuario_Load(object sender, EventArgs e)
         {
+            mobjDrResultado = null;
+        }
 
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            Buscar();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void dgvUsu_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Seleccionar();
+        }
+
+        private void btnSeleccionar_Click(object sender, EventArgs e)
+        {
+            Seleccionar();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Limpiar()
+        {
+            txtNombre.Text = string.Empty;
+            dgvUsu.DataSource = null;
+        }
+
+        private void Buscar()
+        {
+            try
+            {
+                //Cargamos el data_grid con el resultado de la busqueda
+                //dgvUsu.DataSource = InterfazBD.BuscarUsuarios(txtNombre.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Seleccionar()
+        {
+            DataGridViewSelectedRowCollection list = dgvUsu.SelectedRows;
+
+            if (list.Count > 0)
+                mobjDrResultado = ((DataRowView)dgvUsu.SelectedRows[0].DataBoundItem).Row;
+            else
+                MessageBox.Show("Seleccione un Usuario.", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            this.Close();
         }
     }
 }
